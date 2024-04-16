1a31674f27d1faaa14164ca0d9d47eecae65a9e1
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-04-16 21:33:56 +0200

    klart nästan testar att publisera

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
 export class ConvertComponent {
+
+
+  //egenskaper
+  value:number = 0;
+  result:String = "";
+
+  convertcelctofar():void
+  {
+     this.result = (this.value*1.8+32).toString()+" F"; 
+    
+     console.log("res"+this.result);
+  }
+  convertfartocel():void
+  {
+    this.result = (this.value/33.8-32).toString()+" C"; 
+  }
+
+  convertmetertofeet():void
+  {
+    this.result = (this.value*3.2808).toString()+" ft";
+  }
+  
+  convertfeettometer():void
+  {
+    this.result = (this.value/3.2808).toString()+" ft";
+  }
 
 }
