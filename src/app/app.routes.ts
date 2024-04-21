9c570fc5387808c82778bfc7e93072e35402ecf8
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-04-21 20:17:19 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ConvertComponent } from './convert/convert.component';
 
 export const routes: Routes = [
-    {path:"home", component:HomeComponent},
+    {path:"", component:HomeComponent},
     {path:"about",component:AboutComponent},
     {path:"convert",component:ConvertComponent}
 ];
