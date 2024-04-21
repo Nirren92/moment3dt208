0fef4003b2103a4c4c60f804261e17a3e3b2f3e0
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-04-21 20:42:44 +0200

    klart

--- src/app/mainmenu/mainmenu.component.ts
+++ src/app/mainmenu/mainmenu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-mainmenu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './mainmenu.component.html',
   styleUrl: './mainmenu.component.scss'
 })
